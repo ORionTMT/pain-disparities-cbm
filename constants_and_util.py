878c42a556e6fc4f8f0219b9f5fccd09ff05d3ac
ORionTMT <mt3566@columbia.edu>
2024-04-03 00:51:48 -0400

    modifying constants and util py

--- constants_and_util.py
+++ constants_and_util.py
@@ -69,12 +69,12 @@ else:
     REPROCESS_RAW_DATA = True # set this to False if you just want to work with the processed data, and don't need to reprocess it. 
     
     # Please set these paths for your system. 
-    INDIVIDUAL_IMAGES_PATH = '/home/mt3566/pain-disparities/processed_data' # points to the directory which stores the processed data, so you should download the processed data into this folder. If you are reprocessing the raw data, the individual images will be stored in this folder. 
-    FITTED_MODEL_DIR = '/home/mt3566/pain-disparities/fitted_model' # This is where you store the fitted models.  Please create three empty subdirectories in this directory: "configs", "results", and "model_weights". 
+    INDIVIDUAL_IMAGES_PATH = '/home/jacktongmt/pain-disparities-cbm/processed_data' # points to the directory which stores the processed data, so you should download the processed data into this folder. If you are reprocessing the raw data, the individual images will be stored in this folder. 
+    FITTED_MODEL_DIR = '/home/jacktongmt/pain-disparities-cbm/fitted_model' # This is where you store the fitted models.  Please create three empty subdirectories in this directory: "configs", "results", and "model_weights". 
     
     # Only need to set these paths if you are reprocessing raw data. 
     BASE_NON_IMAGE_DATA_DIR = '/home/mt3566/NDA/nonImage' # Set this path to point to the directory where you downloaded the NON-IMAGE OAI data - eg, it should contain folders like "AllClinical_ASCII". 
-    BASE_IMAGE_DATA_DIR = '/home/mt3566/NDA/nda-tools/downloadcmd/packages/1225784' # Set this path to point to the directory where you downloaded the IMAGE OAI data - eg, it should contain folders like "00m" for each timepoint. 
+    BASE_IMAGE_DATA_DIR = '/home/jacktongmt/NDA/nda-tools/downloadcmd/packages/1225783' # Set this path to point to the directory where you downloaded the IMAGE OAI data - eg, it should contain folders like "00m" for each timepoint. 
 
 assert os.path.exists(INDIVIDUAL_IMAGES_PATH), 'You need to set INDIVIDUAL_IMAGES_PATH; see "Please set these paths for your system" comment in constants_and_util.py'
 assert os.path.exists(FITTED_MODEL_DIR), 'You need to set FITTED_MODEL_DIR; see "Please set these paths for your system" comment in constants_and_util.py. After setting this directory, please create empty subdirectories called "configs", "results", and "model_weights" within it'
