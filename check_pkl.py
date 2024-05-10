c17057bf138e04db22c39d803b94b2b711220c2e
ORionTMT <mt3566@columbia.edu>
2024-05-10 15:18:24 -0400

    Fix error handling when loading pickle file

--- check_pkl.py
+++ check_pkl.py
@@ -2,8 +2,8 @@ import pickle
 
 # Open the pickle file in binary read mode
 with open('/home/jacktongmt/pain-disparities-cbm/processed_data_00m/test/show_both_knees_True_downsample_factor_None_normalization_method_our_statistics/image_codes.pkl', 'rb') as file:
-    # Load the data from the pickle file
-    data = pickle.load(file)
-
-
-    print(type(data))
+    try:
+        data = pickle.load(file)
+        print(type(data))
+    except Exception as e:
+        print(f"Error occurred while loading pickle file: {str(e)}")
