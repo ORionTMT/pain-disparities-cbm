f1aca941fe12ff0ad2a8ef1d849d272325bf558d
ORionTMT <mt3566@columbia.edu>
2024-05-10 16:17:48 -0400

    Refactor code for checking pickle file and print length of data

--- check_pkl.py
+++ check_pkl.py
@@ -1,10 +1,10 @@
 import pickle
 
 # Open the pickle file in binary read mode
-with open('/home/jacktongmt/pain-disparities-cbm/processed_data_00m/test/show_both_knees_True_downsample_factor_None_normalization_method_our_statistics/image_codes.pkl', 'rb') as file:
+with open('/home/jacktongmt/pain-disparities-cbm/processed_data_00m/train/show_both_knees_True_downsample_factor_None_normalization_method_our_statistics/image_codes.pkl', 'rb') as file:
     try:
         data = pickle.load(file)
-        
+        print(type(data))
         print(len(data))  # Print the length of the list
         print(data[0]) 
     except Exception as e:
