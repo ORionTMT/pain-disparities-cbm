8b27ed055fdd1ca3e754ed1743d8a3c1674ed121
ORionTMT <mt3566@columbia.edu>
2024-05-10 15:25:10 -0400

    Refactor code for checking pickle file and print length of data

--- check_pkl.py
+++ check_pkl.py
@@ -4,6 +4,8 @@ import pickle
 with open('/home/jacktongmt/pain-disparities-cbm/processed_data_00m/test/show_both_knees_True_downsample_factor_None_normalization_method_our_statistics/image_codes.pkl', 'rb') as file:
     try:
         data = pickle.load(file)
-        print(type(data))
+        
+        print(len(data))  # Print the length of the list
+        print(data[0]) 
     except Exception as e:
         print(f"Error occurred while loading pickle file: {str(e)}")
